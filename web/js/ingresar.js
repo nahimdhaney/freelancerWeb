3949561485dd9e36fc0357962fdc0288d72b9f6a
nahimdhaney <nahimdhaney@hotmail.com>
2018-04-23 09:31:55 -0400

    completando Ingresar con Servicios funcionando

--- web/js/ingresar.js
+++ web/js/ingresar.js
@@ -11,7 +11,7 @@ function resultado(resultado) {
         var url = "index.html"; 
         $(location).attr('href',url);
     }else{
-        $("#respuesta").html("Error al ingresar");        
+        $("#respuesta").html(resultado.mensaje);        
     }
 
 }
@@ -35,72 +35,3 @@ function ingresar() {
     });
 }
 
-function ejecutarQuery() {
-//    var myObject = new Object();
-//myObject.name = "John";
-//myObject.age = 12;
-//myObject.pets = ["cat", "dog"];
-    var nombre = $("#nombre").val();
-    var apellido = $("#Apellido").val();
-    var nombreCompleto = nombre + " " + apellido;
-    var nombreUsuario = $("#nombreUsuario").val();
-    var tarjeta = $("#Tarjeta").val();
-    var direccion = $("#Direccion").val();
-    var contraseña = $("#contraseña").val();
-    var contraseña2 = $("#contraseña2").val();
-    var usuario = new Object();
-    usuario.usuarioId = 1;
-    usuario.nombreCompleto = nombreCompleto;
-    usuario.userName = nombreUsuario;
-    usuario.password = contraseña;
-    usuario.direccion = direccion;
-    usuario.fechaIngreso = "2234";
-    usuario.NroTarjeta = tarjeta;
-    if (contraseña.length < 3) {
-        $("#respuesta").html("Por favor Digite Una contraseña mas larga");
-        return;
-    }
-    if (contraseña != contraseña2) {
-        $("#respuesta").html("Las contraseñas no son iguales");
-        return;
-    }
-
-
-//var valor = {"qty":999,"name":"iPad 3"};
-
-    jQuery.ajax({
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        'type': 'POST',
-        'url': "../api/usuario/login",
-        'data': JSON.stringify(usuario),
-        'dataType': 'json',
-        'success': prueba
-    });
-//   
-//$.postJSON = function(url, data, callback) {
-//    return jQuery.ajax({
-//    headers: { 
-//        'Accept': 'application/json',
-//        'Content-Type': 'application/json' 
-//    },
-//    'type': 'POST',
-//    'url': url,
-//    'data': JSON.stringify(data),
-//    'dataType': 'json',
-//    'success': callback
-//    });
-//};
-
-//       $.post(,usuario,prueba, 'json');
-//    private String userName;
-//    private String password;
-//    private String direccion;
-//    private String fechaIngreso;
-//    private String NroTarjeta;
-
-
-
-}
