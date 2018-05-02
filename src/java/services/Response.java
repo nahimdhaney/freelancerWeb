109f0d9decbe1a358ed106e40dbe0d24089bc463
kevin554 <kevinduran@outlook.com>
2018-05-01 21:52:33 -0400

    La librería Gson y la clase Response para las respuesta de los servicios

---
+++ src/java/services/Response.java
@@ -0,0 +1,36 @@
+package services;
+
+public class Response {
+
+    private boolean success;
+    private String message;
+    private Object response;
+
+    public Response() {
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getResponse() {
+        return response;
+    }
+
+    public void setResponse(Object response) {
+        this.response = response;
+    }
+    
+}
